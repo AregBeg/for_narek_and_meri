9fe56380924774957288ef109eb3ab90b01f08fc
Davit0012 <dadas.dadas.09@mail.ru>
2022-10-31 10:22:07 +0400

    make it better

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const gorgBlock = document.querySelector("#gorg");
+const gorgDivs = document.querySelectorAll("#gorg div")
 const gifNM = document.querySelector("#gif_nm");
 gifNM.src = "./gif/nm2.gif"
 setTimeout(() => {
+  gorgDivs.forEach(div => div.style.display = "block");
   gorgBlock.style.display = "flex";  
   gorgBlock.classList.add("gorg");
 }, 4000);
